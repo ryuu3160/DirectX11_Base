b242cb21f52bec01655dcbd7a2a6cf82c4b6ddb0
ryuu3160(MainPC) <principe2580613@yahoo.co.jp>
2025-07-22 15:15:34 +0900

    feat: 描画用の基底コンポーネント追加

---
+++ DirectX11_Base/Source/DirectX11/System/RenderComponent.cpp
@@ -0,0 +1,47 @@
+/*+===================================================================
+	File: RenderComponent.cpp
+	Summary: 描画用の基底コンポーネント
+	Author: AT13C192 01 青木雄一郎
+	Date: 2025/7/20 Sun PM 10:33:28 初回作成
+===================================================================+*/
+
+// ==============================
+//	include
+// ==============================
+#include "RenderComponent.hpp"
+#include "System/Component/Camera.hpp"
+
+RenderComponent::RenderComponent()
+	: m_nLayer(0), m_pViewCamera(nullptr), m_pCameraObj(nullptr)
+{
+}
+
+RenderComponent::~RenderComponent()
+{
+}
+
+void RenderComponent::SetCamera(_In_ GameObject *In_Camera) noexcept
+{
+	if (NullCheck(In_Camera, NCMode::OUTPUT, "Camera is null in RenderComponent::SetCamera."))
+		return;
+
+	m_pViewCamera = In_Camera->GetComponent<Camera>();
+
+	if(NullCheck(m_pViewCamera, NCMode::OUTPUT, "Camera component is null in RenderComponent::SetCamera."))
+		return;
+
+	m_pCameraObj = In_Camera;
+}
+
+void RenderComponent::ExecuteUpdate() noexcept
+{
+}
+
+void RenderComponent::ExecuteDraw() noexcept
+{
+}
+
+void RenderComponent::ReadWrite(_In_ DataAccessor *In_Data)
+{
+	In_Data->Access<FilePath>(&m_AssetPath);
+}
---
+++ DirectX11_Base/Source/DirectX11/System/RenderComponent.hpp
@@ -0,0 +1,68 @@
+/*+===================================================================
+	File: RenderComponent.hpp
+	Summary: 描画用の基底コンポーネント
+	Author: AT13C192 01 青木雄一郎
+	Date: 2025/7/20 Sun PM 10:33:20 初回作成
+===================================================================+*/
+#pragma once
+
+// ==============================
+//	include
+// ==============================
+#include "System/Component/Component.hpp"
+#include "DirectX11/Texture/Shader.hpp"
+#include "DirectX11/Texture/MeshBuffer.hpp"
+#include "DirectX11/Texture/Texture.hpp"
+
+// ==============================
+//  前方宣言
+// ==============================
+class Camera;
+
+/// <summary>
+/// RenderComponentクラス
+/// </summary>
+class RenderComponent : public Component
+{
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	RenderComponent();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	virtual ~RenderComponent();
+
+	/// <summary>
+	/// ビューカメラを設定します。
+	/// </summary>
+	/// <param name="[In_Camera]">設定するカメラオブジェクトへのポインタ。</param>
+	void SetCamera(_In_ GameObject *In_Camera) noexcept;
+
+	/// <summary>
+	/// アセットのパスを設定します。
+	/// </summary>
+	/// <param name="[In_Path]">設定するファイルパス。</param>
+	inline void SetAssetPath(_In_ const FilePath &In_Path) noexcept { m_AssetPath = In_Path; }
+
+	void ExecuteUpdate() noexcept override;
+
+	void ExecuteDraw() noexcept override;
+
+	/// <summary>
+	/// データアクセサーを使用して読み書きを行います。
+	/// </summary>
+	/// <param name="In_Data">読み書き操作に使用する DataAccessor 型のポインタ。</param>
+	void ReadWrite(_In_ DataAccessor *In_Data) override;
+
+protected:
+	FilePath m_AssetPath;					// アセットパス
+	int m_nLayer;							// レイヤー番号
+	GameObject *m_pCameraObj;				// カメラオブジェクトへのポインタ
+	Camera *m_pViewCamera;					// ビューカメラ
+
+	std::shared_ptr<VertexShader> m_defVS;	// デフォルトの頂点シェーダー
+	std::shared_ptr<PixelShader> m_defPS;	// デフォルトのピクセルシェーダー
+};
