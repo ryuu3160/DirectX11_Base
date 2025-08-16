8aefde1f335665d1d1297e45e548ac28aba076cd
ryuu3160 <principe2580613@yahoo.co.jp>
2025-08-17 01:59:44 +0900

    feat: ShaderManagerファイルのみ追加

---
+++ DirectX11_Base/Source/DirectX11/Resource/ShaderManager.hpp
@@ -0,0 +1,41 @@
+/*+===================================================================
+	File: ShaderManager.hpp
+	Summary: シェーダーを管理するクラス
+	Author: AT13C192 01 青木雄一郎
+	Date: 2025/08/17 Sun AM 01:33:29 初回作成
+===================================================================+*/
+#pragma once
+
+// ==============================
+//	include
+// ==============================
+#include "DirectX11/Texture/Shader.hpp"
+
+/// <summary>
+/// ShaderManagerクラス
+/// </summary>
+class ShaderManager
+{
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	ShaderManager() = default;
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~ShaderManager() = default;
+
+	// ------------------------------
+	//  Getter
+	// ------------------------------
+
+
+	// ------------------------------
+	//  Setter
+	// ------------------------------
+
+private:
+
+};
