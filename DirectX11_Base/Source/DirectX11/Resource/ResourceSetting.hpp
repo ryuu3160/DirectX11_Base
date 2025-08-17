bf20d3532778efb8c55c88045338abeaade22d2b
ryuu3160(MainPC) <principe2580613@yahoo.co.jp>
2025-08-18 04:17:51 +0900

    feat: assimpのテクスチャタイプから自分で作ったテクスチャタイプの列挙型へのコンバート関数追加

--- DirectX11_Base/Source/DirectX11/Resource/ResourceSetting.hpp
+++ DirectX11_Base/Source/DirectX11/Resource/ResourceSetting.hpp
@@ -21,4 +21,31 @@ namespace ResourceSetting
 		aiTextureType_NORMALS,    // ノーマルマップ
 		aiTextureType_EMISSIVE,   // エミッシブ
 	};
+
+	enum TextureType : unsigned int
+	{
+		TextureType_Diffuse = 0,    // ディフューズ
+		TextureType_Specular,       // スペキュラー
+		TextureType_Normal,         // ノーマルマップ
+		TextureType_Emissive,       // エミッシブ
+		TextureType_Max,            // 最大値（配列のサイズ取得用）
+
+		TextureType_Unknown = 0xFFFFFFFF // 未知のテクスチャタイプ
+	};
+
+	inline const TextureType aiTextureTypeToTextureType(_In_ const aiTextureType &In_Type) noexcept
+	{
+		switch (In_Type)
+		{
+		case aiTextureType_DIFFUSE:    return TextureType_Diffuse;
+		case aiTextureType_SPECULAR:   return TextureType_Specular;
+		case aiTextureType_NORMALS:    return TextureType_Normal;
+		case aiTextureType_EMISSIVE:   return TextureType_Emissive;
+
+
+		case aiTextureType_UNKNOWN: // 未知のテクスチャタイプ
+		default:                       return TextureType_Unknown; // 未知のテクスチャタイプ
+		}
+	}
+
 }
