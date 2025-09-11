e44e3ce47589a566f67941ab26aeebea46ac0ec4
ryuu3160(MainPC) <principe2580613@yahoo.co.jp>
2025-09-12 02:51:50 +0900

    feat: シーンの削除を安全に

--- DirectX11_Base/Source/System/Scene/SceneManager.cpp
+++ DirectX11_Base/Source/System/Scene/SceneManager.cpp
@@ -62,7 +62,7 @@ void SceneManager::RootDraw() noexcept
 void SceneManager::UpdateSceneChange() noexcept
 {
 	// シーンの読み込みが無い場合は何もしない
-	if(!m_Futures.empty() && !m_pNextScene && m_NextSubScene.empty())
+	if(!m_Futures.empty() && !m_pNextScene && m_NextSubScene.empty() && m_RemoveSubScene.empty())
 		return;
 
 	// 非同期ロード
@@ -86,45 +86,11 @@ void SceneManager::UpdateSceneChange() noexcept
 		m_pNextScene = nullptr;
 	}
 
+	// サブシーンの削除
+	_RemoveSubScene();
+
 	// サブシーンの切り替え&追加
-	for (auto &itr : m_NextSubScene)
-	{
-		for (auto &cur : m_SubScene)
-		{
-			// 同じ型のサブシーンがある場合は置き換え
-			if (cur.first == itr.first)
-			{
-				cur.second->Uninit();
-				cur.second.reset();
-				cur.second = itr.second;
-				continue;
-			}
-
-			// 見つからなかった場合は追加
-			m_SubScene.push_back({ itr.first,itr.second });
-			itr.second.reset();
-			itr.second = nullptr;
-		}
-	}
-	m_NextSubScene.clear();
-}
-
-void SceneManager::RemoveSubScene(_In_ const std::type_index &In_Type) noexcept
-{
-	for (auto itr = m_SubScene.begin(); itr != m_SubScene.end(); ++itr)
-	{
-		if (itr->first == In_Type)
-		{
-			if (itr->second)
-			{
-				itr->second->Uninit();
-				itr->second.reset();
-				itr->second = nullptr;
-			}
-			m_SubScene.erase(itr);
-			return;
-		}
-	}
+	_ChangeAndAddSubScene();
 }
 
 void SceneManager::RemoveAllSubScene() noexcept
@@ -179,4 +145,56 @@ void SceneManager::_RootUpdateLate() noexcept
 		if (itr.second)
 			itr.second->_RootUpdateLate();
 	}
+}
+
+void SceneManager::_RemoveSubScene() noexcept
+{
+	// 削除予約リストを元にサブシーンを削除
+	for (auto &itr : m_RemoveSubScene)
+	{
+		// サブシーンを全て検索
+		for (auto SubItr = m_SubScene.begin(); SubItr != m_SubScene.end();)
+		{
+			// 削除予約リストにある型情報と一致した場合は削除
+			if (SubItr->first == itr)
+			{
+				if (SubItr->second)
+				{
+					SubItr->second->Uninit();
+					SubItr->second.reset();
+					SubItr->second = nullptr;
+					SubItr = m_SubScene.erase(SubItr);
+				}
+				else
+				{
+					++SubItr;
+				}
+			}
+		}
+	}
+	m_RemoveSubScene.clear(); // 削除予約リストをクリア
+}
+
+void SceneManager::_ChangeAndAddSubScene() noexcept
+{
+	for (auto &itr : m_NextSubScene)
+	{
+		for (auto &cur : m_SubScene)
+		{
+			// 同じ型のサブシーンがある場合は置き換え
+			if (cur.first == itr.first)
+			{
+				cur.second->Uninit();
+				cur.second.reset();
+				cur.second = itr.second;
+				continue;
+			}
+
+			// 見つからなかった場合は追加
+			m_SubScene.push_back({ itr.first,itr.second });
+			itr.second.reset();
+			itr.second = nullptr;
+		}
+	}
+	m_NextSubScene.clear();
 }
