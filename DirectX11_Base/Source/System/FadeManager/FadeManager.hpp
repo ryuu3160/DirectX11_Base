0540572db87d1b3892547108502858d19bd35024
ryuu3160(MainPC) <principe2580613@yahoo.co.jp>
2025-09-14 04:13:17 +0900

    feat: フェードマネージャ―完成

--- DirectX11_Base/Source/System/FadeManager/FadeManager.hpp
+++ DirectX11_Base/Source/System/FadeManager/FadeManager.hpp
@@ -29,13 +29,16 @@ public:
 	struct FadeInfo
 	{
 	public:
-		float FadeTime;
 		bool IsAutoDelete;
-		FilePathHold TexturePath{};
-		FilePathHold PixelShaderPath{};
-		FilePathHold VertexShaderPath{};
+		bool IsStartFadeIn;
+		bool IsStartFadeOut;
+		Ease::EasingType EaseType;
+		FilePath TexturePath{};
+		FilePathHold PixelShaderName{};
+		FilePathHold VertexShaderName{};
 	private:
 		friend class FadeManager;
+		Ease::DataDuration EaseData;
 		GameObject *pFadeObj = nullptr;
 	};
 
@@ -45,25 +48,63 @@ public:
 
 	void Draw() noexcept;
 
-	void AddFade(_In_ std::string_view In_Name, _In_ float In_FadeTime, _In_ bool In_IsAutoDelete = false) noexcept;
+	/// <summary>
+	/// 指定した名前のフェードを追加します。
+	/// </summary>
+	/// <param name="[In_Name]">フェード名</param>
+	/// <param name="[In_FadeTime]">フェードの時間（秒単位）を指定します</param>
+	/// <param name="[In_IsAutoDelete]">フェード完了後に自動的に削除するかどうかを指定します。デフォルトは false です</param>
+	void AddFade(_In_ std::string_view In_Name, _In_ float In_FadeTime, _In_ Ease::EasingType In_EaseType = Ease::LERP, _In_ bool In_IsAutoDelete = false,
+		_In_ FilePath In_TexturePath = "", _In_ std::string_view In_PixcelShaderName = "", _In_ std::string_view In_VertexShaderName = "") noexcept;
 
-	void AddFade(_In_ std::string_view In_Name, _In_ const FadeInfo &In_FadeInfo) noexcept;
+	/// <summary>
+	/// 指定された名前にフェード情報を追加します。
+	/// </summary>
+	/// <param name="[In_Name]">フェード名</param>
+	/// <param name="[In_FadeTime]">フェードの時間（秒単位）を指定します</param>
+	/// <param name="[In_FadeInfo]">追加するフェード情報</param>
+	void AddFade(_In_ std::string_view In_Name, _In_ float In_FadeTime, _In_ const FadeInfo &In_FadeInfo) noexcept;
 
+	/// <summary>
+	/// 指定された名前のフェードイン処理を開始します。
+	/// </summary>
+	/// <param name="[In_Name]">フェード名</param>
 	void StartFadeIn(_In_ std::string_view In_Name) noexcept;
 
+	/// <summary>
+	/// 指定された名前のフェードアウト処理を開始します。
+	/// </summary>
+	/// <param name="[In_Name]">フェード名</param>
 	void StartFadeOut(_In_ std::string_view In_Name) noexcept;
 
+	/// <summary>
+	/// 指定された名前のフェード処理を停止します。
+	/// </summary>
+	/// <param name="[In_Name]">フェード名</param>
 	void StopFade(_In_ std::string_view In_Name) noexcept;
 
+	/// <summary>
+	/// 指定された名前のフェードを削除します。
+	/// </summary>
+	/// <param name="[In_Name]">フェード名</param>
 	void DeleteFade(_In_ std::string_view In_Name) noexcept;
 
 private:
 	FadeManager();
 	~FadeManager();
 
+	void UpdateFadeIn(_Inout_ FadeInfo &InOut_FadeInfo) noexcept;
+	void UpdateFadeOut(_Inout_ FadeInfo &InOut_FadeInfo) noexcept;
+
+	/// <summary>
+	/// 指定された名前とフェード情報を使用して、フェード用のGameObjectを作成します。
+	/// </summary>
+	/// <param name="[In_Name]">フェード名</param>
+	/// <param name="[In_FadeInfo]">フェード効果の詳細情報を含むFadeInfo構造体。</param>
+	/// <returns>作成されたフェード用GameObjectへのポインタ。</returns>
 	GameObject *CreateFadeObj(_In_ std::string_view In_Name, _In_ const FadeInfo &In_FadeInfo);
 
 private:
 	GameObject *m_pCameraObj;
-	std::unordered_map<std::string,FadeInfo> m_FadeObj;
+	std::unordered_map<std::string,FadeInfo> m_mapFadeObj;
 };
