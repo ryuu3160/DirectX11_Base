ea9d36bff1fc6f8b9cff5908d87040e35492c024
ryuu3160 <principe2580613@yahoo.co.jp>
2025-10-13 23:56:51 +0900

    feat: Object追加

---
+++ DirectX11_Base/Source/System/Object/Object.hpp
@@ -0,0 +1,37 @@
+/*+===================================================================
+	File: Object.hpp
+	Summary: オブジェクトの基底クラス
+	Author: AT13C192 01 青木雄一郎
+	Date: 2025/10/13 Mon PM 11:45:55 初回作成
+===================================================================+*/
+#pragma once
+
+// ==============================
+//	include
+// ==============================
+
+// ==============================
+//	前方宣言
+// ==============================
+class ColliderBase;
+
+/// <summary>
+/// Objectクラス
+/// </summary>
+class Object
+{
+public:
+	Object() = default;
+	~Object() = default;
+
+protected:
+	virtual void Awake() noexcept {};
+	virtual void Init() noexcept {};
+	virtual void Update() noexcept {};
+	virtual void LateUpdate() noexcept {};
+
+	// 当たり判定用
+	virtual void OnCollisionEnter(_In_ ColliderBase *In_Other) noexcept {};
+	virtual void OnCollisionStay(_In_ ColliderBase *In_Other) noexcept {};
+	virtual void OnCollisionExit(_In_ ColliderBase *In_Other) noexcept {};
+};
