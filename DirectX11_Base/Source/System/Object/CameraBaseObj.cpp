1811da98b333256fc6e2904780c13cc587aebbaf
ryuu3160(MainPC) <principe2580613@yahoo.co.jp>
2025-11-08 23:59:30 +0900

    feat: カメラオブジェのベース追加

---
+++ DirectX11_Base/Source/System/Object/CameraBaseObj.cpp
@@ -0,0 +1,22 @@
+/*+===================================================================
+	File: CameraBaseObj.cpp
+	Summary: カメラオブジェクトのベース
+	Author: AT13C192 01 青木雄一郎
+	Date: 2025/11/8 Sat PM 11:36:43 初回作成
+===================================================================+*/
+
+// ==============================
+//	include
+// ==============================
+#include "CameraBaseObj.hpp"
+
+CameraBaseObj::CameraBaseObj(_In_ std::string In_Name)
+	: GameObject(In_Name)
+	, m_pComponent(nullptr)
+{
+	m_pComponent = AddComponent<Camera>();
+}
+
+CameraBaseObj::~CameraBaseObj()
+{
+}
