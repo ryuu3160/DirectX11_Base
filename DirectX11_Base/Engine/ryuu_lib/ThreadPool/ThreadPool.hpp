08c81da0bdd63afc3dae1f02db5e21be147ce88d
ryuu3160(MainPC) <principe2580613@yahoo.co.jp>
2026-04-10 23:52:03 +0900

    feat: スレッドプール更新

--- DirectX11_Base/Engine/ryuu_lib/ThreadPool/ThreadPool.hpp
+++ DirectX11_Base/Engine/ryuu_lib/ThreadPool/ThreadPool.hpp
@@ -12,9 +12,9 @@
 // ==============================
 #include "ryuu_lib/Singleton/Singleton.hpp"
 
-/// <summary>
-/// ThreadPoolクラス
-/// </summary>
+/**
+ * @brief スレッドプール
+ */
 class ThreadPool : public Singleton<ThreadPool>
 {
     friend class Singleton<ThreadPool>;
@@ -62,71 +62,44 @@ public:
     };
 
 public:
-
-    /// <summary>
-    /// タスク追加(Shutdown開始後は拒否してfalseを返す)
-    /// </summary>
+	
+    /**
+	 * @brief タスク追加(Shutdown開始後は拒否してfalseを返す)
+	 * @tparam T タスクの型(呼び出し可能オブジェクト)
+	 * @param In_Task 実行したいタスク(引数無しの呼び出し可能オブジェクト。ラムダ式もOK)
+	 * @return タスクハンドル(受付拒否ならstd::nullopt)
+     */
     template <typename T>
-    std::optional<TaskHandle> AddTask(_In_ T &&In_Task)
-    {
-        if(!m_AcceptTasks.load(std::memory_order_acquire))
-            return std::nullopt;
+    std::optional<TaskHandle> AddTask(_In_ T &&In_Task);
 
-        m_PendingTasks.fetch_add(1, std::memory_order_relaxed);
+    /**
+	 * @brief タスク追加(引数付きオーバーロード)
+	 * @brief 例1: AddTask(Function, arg1, arg2);
+	 * @brief 例2: AddTask(&MyClass::MemberFunction, &obj, arg1, arg2);
+	 * @tparam T タスクの型(呼び出し可能オブジェクト)
+	 * @tparam ...Args タスクの引数の型
+	 * @param In_Task 実行したいタスク
+	 * @param ...In_Args タスクの引数
+	 * @return タスクハンドル(受付拒否ならstd::nullopt)
+     */
+    template <typename T, typename... Args>
+	std::optional<TaskHandle> AddTask(_In_ T &&In_Task, _In_ Args&&... In_Args);
 
-        auto state = std::make_shared<TaskHandle::State>();
-        TaskHandle handle{ state };
-
-        auto wrapped = [this, state, task = std::forward<T>(In_Task)]() mutable
-            {
-                try { task(); }
-                catch(...)
-                {
-					OutputDebugStringA("error : ThreadPool: Unhandled exception in task\n");
-                }
-
-                // タスク完了を通知
-                state->done.store(true, std::memory_order_release);
-                state->cv.notify_all();
-
-                OnTaskFinished();
-            };
-
-        const int idx = GetCurrentThreadIndex();
-
-        if(idx >= 0)
-        {
-            m_Workers[static_cast<std::size_t>(idx)]->PushBack(std::move(wrapped));
-            WakeupAll(idx);
-        }
-        else
-        {
-            {
-                std::unique_lock<std::mutex> lock(m_GlobalMutex);
-                m_GlobalTasks.emplace_back(std::move(wrapped));
-            }
-            m_GlobalWakeSeq.fetch_add(1, std::memory_order_release);
-            m_GlobalCond.notify_all();
-            WakeupAll(-1);
-        }
-
-        return handle;
-    }
-
-    /// <summary>
-    /// (受付停止後に)全タスク完了まで待つ
-    /// </summary>
+    /**
+	 * @brief (受付停止後に)全タスク完了まで待つ
+     */
     void WaitUntilIdle();
-
-    /// <summary>
-    /// シャットダウン(Drain/Terminate)
-    /// </summary>
+	
+    /**
+	 * @brief シャットダウン
+	 * @param In_Mode シャットダウンモード(Drain/Terminate)
+     */
     void Shutdown(_In_ ShutdownMode In_Mode);
 
-    /// <summary>
-	/// タスク受付中か(Shutdown開始後はfalseになる)
-    /// </summary>
-	/// <returns>trueなら受付中、falseなら受付停止</returns>
+    /**
+	 * @brief タスク受付中か(Shutdown開始後はfalseになる)
+	 * @return trueなら受付中、falseなら受付停止
+     */
     inline bool IsAcceptingTasks() const
     {
         return m_AcceptTasks.load(std::memory_order_acquire);
@@ -181,24 +154,28 @@ private:
     ~ThreadPool();
 
     // TLSから取得(ワーカースレッド以外は -1)
+
+    /**
+	 * @brief 現在のスレッドがワーカースレッドならそのIndexを、そうでないなら-1を返す
+     */
     int GetCurrentThreadIndex() const;
 
-    /// <summary>
-	/// タスクを盗むかグローバルから引っ張る(ワーカースレッドは自分以外のワーカーから盗み、ワーカースレッド以外はグローバルから引っ張る)
-    /// </summary>
-	/// <param name="[In_Index]">ワーカーのIndex(ワーカースレッド以外は-1)</param>
-	/// <returns>タスク(無ければ空)</returns>
+    /**
+	 * @brief タスクを盗むかグローバルから引っ張る(ワーカースレッドは自分以外のワーカーから盗み、ワーカースレッド以外はグローバルから引っ張る)
+	 * @param In_Index ワーカーのIndex(ワーカースレッド以外は-1)
+	 * @return タスク(無ければ空)
+     */
     std::function<void()> TaskStealOrPull(_In_ int In_Index);
 
-    /// <summary>
-	/// 全ワーカーを起こす(ワーカースレッド以外は全て、ワーカースレッドは自分以外を起こす)
-    /// </summary>
-	/// <param name="[In_Index]">ワーカーのIndex(ワーカースレッド以外は-1)</param>
+    /**
+	 * @brief 全ワーカーを起こす(ワーカースレッド以外は全て、ワーカースレッドは自分以外を起こす)
+	 * @param In_Index ワーカーのIndex(ワーカースレッド以外は-1)
+     */
     void WakeupAll(_In_ int In_Index);
 
-    /// <summary>
-	/// タスク完了通知(タスクが完了したら呼ぶ。pending減算と、待ち状態なら起こすのを行う)
-    /// </summary>
+    /**
+	 * @brief タスク完了通知(タスクが完了したら呼ぶ。pending減算と、待ち状態なら起こすのを行う)
+     */
     void OnTaskFinished();
 
 private:
@@ -214,3 +191,111 @@ private:
     mutable std::mutex m_GlobalMutex;
     std::condition_variable m_GlobalCond;
 };
+
+template<typename T>
+inline std::optional<ThreadPool::TaskHandle> ThreadPool::AddTask(_In_ T &&In_Task)
+{
+    if(!m_AcceptTasks.load(std::memory_order_acquire))
+        return std::nullopt;
+
+    m_PendingTasks.fetch_add(1, std::memory_order_relaxed);
+
+    auto state = std::make_shared<TaskHandle::State>();
+    TaskHandle handle{ state };
+
+    auto wrapped = [this, state, task = std::forward<T>(In_Task)]() mutable
+        {
+            try { task(); }
+            catch(...)
+            {
+                OutputDebugStringA("error : ThreadPool: Unhandled exception in task\n");
+            }
+
+            // タスク完了を通知
+            state->done.store(true, std::memory_order_release);
+            state->cv.notify_all();
+
+            OnTaskFinished();
+        };
+
+    const int idx = GetCurrentThreadIndex();
+
+    if(idx >= 0)
+    {
+        m_Workers[static_cast<std::size_t>(idx)]->PushBack(std::move(wrapped));
+        WakeupAll(idx);
+    }
+    else
+    {
+        {
+            std::unique_lock<std::mutex> lock(m_GlobalMutex);
+            m_GlobalTasks.emplace_back(std::move(wrapped));
+        }
+        m_GlobalWakeSeq.fetch_add(1, std::memory_order_release);
+        m_GlobalCond.notify_all();
+        WakeupAll(-1);
+    }
+
+    return handle;
+}
+
+template<typename T, typename ...Args>
+inline std::optional<ThreadPool::TaskHandle> ThreadPool::AddTask(_In_ T &&In_Task, _In_ Args && ...In_Args)
+{
+	if(!m_AcceptTasks.load(std::memory_order_acquire))
+		return std::nullopt;
+
+	m_PendingTasks.fetch_add(1, std::memory_order_relaxed);
+
+	auto state = std::make_shared<TaskHandle::State>();
+	TaskHandle handle{ state };
+
+	// In_TaskとIn_Argsを値として保持して、実行時にinvokeする
+	using Fnc = std::decay_t<T>;
+	using Tup = std::tuple<std::decay_t<Args>...>;
+
+	auto fnc = Fnc(std::forward<T>(In_Task));
+	auto tup = Tup(std::forward<Args>(In_Args)...);
+
+	auto wrapped = [this, state, fnc = std::move(fnc), tup = std::move(tup)]() mutable
+		{
+			try
+			{
+				std::apply(
+					[&](auto&&... unpacked)
+					{
+						std::invoke(fnc, std::forward<decltype(unpacked)>(unpacked)...);
+					},
+					tup
+				);
+			}
+			catch(...)
+			{
+				OutputDebugStringA("error : ThreadPool: Unhandled exception in task\n");
+			}
+
+			state->done.store(true, std::memory_order_release);
+			state->cv.notify_all();
+
+			OnTaskFinished();
+		};
+
+	const int idx = GetCurrentThreadIndex();
+	if(idx >= 0)
+	{
+		m_Workers[static_cast<std::size_t>(idx)]->PushBack(std::move(wrapped));
+		WakeupAll(idx);
+	}
+	else
+	{
+		{
+			std::unique_lock<std::mutex> lock(m_GlobalMutex);
+			m_GlobalTasks.emplace_back(std::move(wrapped));
+		}
+		m_GlobalWakeSeq.fetch_add(1, std::memory_order_release); // ←あなたのwakeSeq方式
+		m_GlobalCond.notify_all();
+		WakeupAll(-1);
+	}
+
+	return handle;
+}
