cd081cd31161cfbceb576a3456f2277ab84aaba7
ryuu3160 <principe2580613@yahoo.co.jp>
2026-06-10 19:01:15 +0900

    refactor: HashGenerator更新

--- DirectX11_Base/Engine/ryuu_lib/CRC31_HashGenerator/HashGenerator.hpp
+++ DirectX11_Base/Engine/ryuu_lib/CRC31_HashGenerator/HashGenerator.hpp
@@ -21,14 +21,32 @@ namespace
 /// </summary>
 class HashGenerator
 {
+private:
+	// CRC-32(IEEE 802.3)逆多項式
+	static constexpr uint32_t CRC32_POLY = 0xEDB88320u;
 public:
 	/// <summary>
 	/// コンストラクタ
 	/// </summary>
-	HashGenerator();
+	HashGenerator()
+	{
+		if(!m_bInitialized)
+		{
+			InitializeCrc32Table();
+			m_bInitialized = true;
+		}
+	}
 	~HashGenerator() = default;
 
-	uint32_t GenerateHash(_In_ std::string_view In_String) const noexcept;
+	uint32_t GenerateHash(_In_ std::string_view In_String) const noexcept
+	{
+		uint32_t c = 0xFFFFFFFF;
+		for(size_t i = 0; i < In_String.length(); i++)
+		{
+			c = m_ui32Crc32Table[(c ^ In_String[i]) & 0xFF] ^ (c >> 8);
+		}
+		return c ^ 0xFFFFFFFF;
+	}
 
 	size_t operator()(std::string_view key) const noexcept
 	{
@@ -40,10 +58,21 @@ public:
 
 
 private:
-	static void InitializeCrc32Table();
+	static void InitializeCrc32Table()
+	{
+		for(uint32_t i = 0; i < 256; ++i)
+		{
+			uint32_t c = i;
+			for(int j = 0; j < 8; ++j)
+			{
+				c = (c & 1) ? (CRC32_POLY ^ (c >> 1)) : (c >> 1);
+			}
+			m_ui32Crc32Table[i] = c;
+		}
+	}
 private:
-	static bool m_bInitialized;
-	static uint32_t m_ui32Crc32Table[CRC32_TABLE_SIZE];
+	static inline bool m_bInitialized = false;
+	static inline uint32_t m_ui32Crc32Table[CRC32_TABLE_SIZE] = { 0 };
 };
 
 struct StringEq {
